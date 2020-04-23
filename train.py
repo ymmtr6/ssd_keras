527cee2bab8c87d33fecb1e374b1525651e8512d
Riku Yamamoto <ymmtr666@gmail.com>
2020-04-24 01:18:41 +0900

    追加学習を分岐させた

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import keras
 from keras.applications.imagenet_utils import preprocess_input
 from keras.backend.tensorflow_backend import set_session
@@ -221,16 +222,18 @@ gen = Generator(gt, bbox_util, 4, path_prefix,
                 (input_shape[0], input_shape[1]), do_crop=False)
 
 model = SSD300(input_shape, num_classes=NUM_CLASSES)
-model.load_weights(cf.WEIGHTS, by_name=True)
+# 追加学習
+if os.path.exists(cf.WEIGHTS):
+    model.load_weights(cf.WEIGHTS, by_name=True)
 
-freeze = ['input_1', 'conv1_1', 'conv1_2', 'pool1',
-          'conv2_1', 'conv2_2', 'pool2',
-          'conv3_1', 'conv3_2', 'conv3_3', 'pool3']  # ,
-#           'conv4_1', 'conv4_2', 'conv4_3', 'pool4']
-
-for L in model.layers:
-    if L.name in freeze:
-        L.trainable = False
+    # 事前学習は入力層を固定する。
+    freeze = ['input_1', 'conv1_1', 'conv1_2', 'pool1',
+              'conv2_1', 'conv2_2', 'pool2',
+              'conv3_1', 'conv3_2', 'conv3_3', 'pool3']  # ,
+    #           'conv4_1', 'conv4_2', 'conv4_3', 'pool4']
+    for L in model.layers:
+        if L.name in freeze:
+            L.trainable = False
 
 
 def schedule(epoch, decay=0.9):
